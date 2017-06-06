fa4fec59a6eaa7e26fad0afbdd4d1be69cb927a0
patjas <jasniewskipatryk@gmail.com>
2017-06-06 13:00:07 +0200

    Dodano nową klasę związaną z planszą (po dłuższej przerwie związanej z licenc.), zmodyfikowano poprzednią.

--- Sudoku/Sudoku/Plansza.cs
+++ Sudoku/Sudoku/Plansza.cs
@@ -13,11 +13,12 @@ namespace Sudoku
 		PlanszaGra r_gra;
 		public bool set=true;
 		private Table tab = new Table(9,9,false);
+		private List<Label>SmallDrafts = new List<Label>();
 
 		public Plansza (PlanszaGra rodzic)
 		{
 			Array.Clear (czysc, 0, czysc.length);
-			p_gra = rodzic;
+			r_gra = rodzic;
 			ustalony = new EventBox ();
 			this.ustalony.Events |= Gdk.EventMask.ButtonPressMask;
 			this.ustalony.ButtonPressEvent += klikniete;
@@ -29,7 +30,7 @@ namespace Sudoku
 
 		private void klikniete(System.Object obj, EventArgs args)
 		{
-			foreach (planszbox pb in p_gra.k_boxes) {
+			foreach (PlanszBox pb in r_gra.k_boxes) {
 				foreach (Plansza pl in pb.lista) {
 					if (f.set == false)
 						f.ustalony.ModifyBg (StateType.Normal, Globals.nieaktywny);
@@ -42,8 +43,8 @@ namespace Sudoku
 			else
 				this.ustalony.ModifyBg (StateType.Normal, Globals.aktyw_zabl);
 
-			p_gra.wybrany = this;
-			foreach (Planszbox pb in p_gra.k_boxes) {	
+			r_gra.wybrany = this;
+			foreach (PlanszBox pb in r_gra.k_boxes) {	
 				foreach (Plansza p in pb.lista) {
 					if (p == this)
 						continue;
@@ -51,21 +52,6 @@ namespace Sudoku
 						p.ustalony.ModifyBg (StateType.Normal, Globals.samenum);
 				}
 			}
-		}
-		private void aktualizuj()
-		{
-			int h=this.ramka.Allocation.Height;
-			if (this.set = false) 
-			{
-				if (true) 
-				{
-					Pango.FontDescription fd = new FontDescription ();
-					fd.Size = Convert.ToInt32 (h * 0.2 * Pango.Scale.PangoScale);
-					this.tab.SetSizeRequest (0, 0);
-
-				}
-			}
-
 		}
 			
 	}
