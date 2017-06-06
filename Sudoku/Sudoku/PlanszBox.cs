86d235b4b8d2a1966fd4762d2f9b43af1c93e120
patjas <jasniewskipatryk@gmail.com>
2017-06-06 12:58:26 +0200

    Dodano nową klasę związaną z planszą (po dłuższej przerwie związanej z licenc.), zmodyfikowano poprzednią.

---
+++ Sudoku/Sudoku/PlanszBox.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Gtk;
+using System.Collections.Generic;
+
+namespace Sudoku
+{
+	public partial class PlanszBox
+	{	private Frame pRamka;
+		public List<Plansza> lista=new List<Plansza>();
+		PlanszGra p_gra;
+
+		public PlanszBox (PlanszaGra rodzic)
+		{
+			p_gra = rodzic;
+			Table pTab = new Table (9,9,false);
+			for (int i = 0; i < 9; i++) 
+			{
+				lista.Add (new PlanszaGra(p_gra));
+				pTab.Attach (lista [i].ustalony, (int)i % 3, (int)i % 3 + 1, (int)Math.Floor ((double)i / 3d),
+					(int)Math.Floor ((double)i / 3d) + 1);
+				pRamka = new Frame ();
+				pRamka.Add (pTab);
+			}
+				
+
+
+		}
+	}
+}
+
